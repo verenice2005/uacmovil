34dc6f9e1cbe0d275708d079102df0bef3953ccd
verenice2005 <veree200594@gmail.com>
2020-05-09 16:11:35 -0500

    v.1.3 Navbar

--- index.php
+++ index.php
@@ -2,9 +2,34 @@
 <html>
 <head>
 	<title>UACMovil</title>
+	<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 </head>
 <body>
- <h1>UACMovil - prueba desde servidor ubuntu</h1>
+ 
+  <nav class="teal">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">UACMovil</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  
+        
+
+
+
+ <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 </body>
 </html>
