db7f30d9e35796c4c0f86f7395283da67542ec76
verenice2005 <veree200594@gmail.com>
2020-05-09 17:11:58 -0500

    v.1.4

--- index.php
+++ index.php
@@ -24,12 +24,28 @@
     </div>
   </nav>
 
-  
+  <!--  pagina principal-->
+
+    <div class="parallax-container" >
+      <div class="parallax"><img src="images/DSC_0439.JPG"></div>
+    </div>
+    <div class="section white">
+      <div class="row container">
+        <h2 class="header">Parallax</h2>
+        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      </div>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="images/DSC_0463.JPG"></div>
+    </div>
+     <!--  termina pagina principal-->   
         
 
 
 
  <!--JavaScript at end of body for optimized loading-->
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
       <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+      <script type="text/javascript" src="materialize/js/scripts.js"></script>
 </body>
 </html>
